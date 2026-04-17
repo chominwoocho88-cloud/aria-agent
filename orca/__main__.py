b6c0f4595d6ecfb1658776089709a812a386f557
chominwoocho88-cloud <chominwoo.cho88@gmail.com>
2026-04-17 22:29:54 +0900

    Delete orca directory

--- orca/__main__.py
+++
@@ -1,7 +0,0 @@
-"""Allow `python -m orca` to run the main ORCA workflow."""
-
-from .main import main
-
-
-if __name__ == "__main__":
-    main()
--- orca/brand.py
+++
@@ -1,8 +0,0 @@
-﻿"""Shared project branding constants."""
-
-ORCA_NAME = "ORCA"
-ORCA_FULL_NAME = "Omnidirectional Risk & Context Analyzer"
-
-JACKAL_NAME = "JACKAL"
-JACKAL_FULL_NAME = "Just-in-time Alert for Candidates & Key Asset Leverage"
-
